c7ff06d6d5ce98849349a297471ee5973a42a10a
AntonioLoFu <antoniolofu99@gmail.com>
2021-02-26 00:48:35 +0100

    Arbolito, que bonito

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -2,9 +2,11 @@
 
 public static class Game
 {
+
     public static Dictionary<int, string> Items = new Dictionary<int, string>()
     {
-        { 0, null },
-        { 1, "Dirt" }
+        {0, null},
+        {1, "Dirt"},
+        {2, "Wood"},
     };
 }
